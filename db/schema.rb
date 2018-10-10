89610c4b409d3884b599f3d13a19eb93b082637c
hiro.honma <jioaporo@gmail.com>
2018-10-10 08:48:50 +0000

    CRUD tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010004947) do
+ActiveRecord::Schema.define(version: 20181010081623) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20181010004947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "status"
   end
 
 end
